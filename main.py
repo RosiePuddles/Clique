0667dd47fd51cc52a1ae336f362bf099635a9e25
nxe <rosiegbartlett@gmail.com>
2022-07-10 17:14:18 +0100

    a lil mistake

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def NeighbourhoodGroup(graph: {int: {int}}) -> int:
 			for n2 in graph[n1]:
 				I = graph[n1].intersection(graph[n2])
 				t = 0
-				while len(I) != 0 and t < 10:
+				while len(I) != 0:
 					n3 = list(I)[0]
 					t = max(t, len(I.intersection(graph[n3])) + 1)
 					I -= graph[n3].union({n3})
